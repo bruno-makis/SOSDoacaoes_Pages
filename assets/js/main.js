ad5809911339c8d31cf1daa840401e8b9b72148a
bruno-makis <110430914+bruno-makis@users.noreply.github.com>
2024-11-06 19:50:52 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,115 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".objetivoprj",
+  `<p> Temos como objetivo, por meio desta aplicação, tornar o processo de ajuda ao próximo, algo mais viável e cômodo de acordo com a vivência corriqueira do dia a dia.</p>
+   <p></p>
+   <p> O intuito é fazer com que o usuário seja um doador de alimentos, remédios ou vestuário, sem precisar se locomover até um ponto de entrega específico. Por meio da abertura de um ticket, um de nossos voluntários irá entrar em contato e combinar a coleta no local que desejar.</p>
+   <p></p>
+   <p> Em contrapartida, a aplicação também possibilitará às pessoas que têm um tempo disponível ou queiram participar desta causa a se inscreverem como voluntários no projeto.</p>
+   <p> Dito isso, você poderá escolher de maneira mais fácil e segura como fazer a diferença na vida de muitas pessoas.</p>`,
+  "Quais os nossos objetivos com essa aplicação?",
+  "SOS Doações",
+  "2024"
+);
+
+hoverChangeExperience(
+  ".githubprj",
+  `Acesso por meio do link: https://github.com/bruno-makis/App_SOSDoa-es.`,
+  "Repositório Git de todo desenvolvimento",
+  "Github",
+  "2024"
+);
+
+hoverChangeExperience(
+  ".documentacaoprj",
+  `Acesso por meio do link: https://docs.google.com/document/d/1obeu4izWOueBfJ0AiGd5MFAje6-y7qLj/edit?usp=drive_link&ouid=109146393713465893731&rtpof=true&sd=true`,
+  "Documentação Técnica da aplicação",
+  "Documentação",
+  "2024"
+  );
+
+hoverChangeExperience(
+  ".versaoprj",
+  `Mendix Studio 10.4.1`,
+  "Versão Utilizada",
+  "Mendix",
+  "2024"
+  );
+
+  hoverChangeExperience(
+    ".umlprj",
+    `Acesso por meio do link: https://i.imgur.com/zS73xYU.jpeg`,
+    "Diagrama",
+    "UML",
+    "2024"
+    );
+
+    hoverChangeExperience(
+      ".gravacaoprj",
+      `Acesso por meio do link: https://www.youtube.com/watch?v=4sr7qAYAW5Q&authuser=0`,
+      "Apresentação da aplicação",
+      "Youtube",
+      "2024"
+      );
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem de marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
+);
+hoverChangeDescription(
+  ".sass",
+  "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
+);
+hoverChangeDescription(
+  ".react",
+  "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
+);
+hoverChangeDescription(
+  ".next",
+  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
+);
+hoverChangeDescription(
+  ".styled",
+  "styled-components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+);
+hoverChangeDescription(
+  ".tailwind",
+  "Tailwind CSS é um framework CSS que nos fornece classes utilitárias com o propósito de estilizar as páginas."
+);
+hoverChangeDescription(
+  ".typescript",
+  "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
+);
+hoverChangeDescription(
+  ".radix",
+  "Radix é uma biblioteca que fornece componentes acessíveis e sem estilos para a criação de aplicação em React."
+);
+hoverChangeDescription(
+  ".cypress",
+  "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
+);
+hoverChangeDescription(
+  ".storybook",
+  "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+);
+
+
+
+
